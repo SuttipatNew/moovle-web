b66ec7277765b59242f050e82502de941fa10c32
YAYAA <Apitchaya@Apitchaya.local>
2018-10-17 18:55:53 +0700

    add field & button

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
+import 
+  { Grid,
+    Button,
+    Container,
+    Form,
+  } from 'semantic-ui-react'
+import styled from 'styled-components'
 
-class GoogleMap extends Component {
+class Homepage extends Component {
+  state = { item: ''}
+
+  handleChange = (e, { item, value }) => this.setState({ [item]: value })
+
+  handleSubmit = () => {
+    const { item } = this.state
+
+    this.setState({ submittedItem: item })
+  }
+
   render() {
+    const { item, submittedItem } = this.state
     return (
-      <div>
-        Moovle!
-        <Button content='Click Here' />
-      </div>
+      <Container>
+        <Layout>
+          <Grid centered>
+            <Grid.Row>
+              Moovle!
+            </Grid.Row>
+            <Grid.Row>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Input placeholder='Search..' item='item' value={ item } onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Button color='red' content='Search' />
+              </Form>
+            </Grid.Row>
+          </Grid>
+        </Layout>
+      </Container>
     );
   }
 }
 
-export default GoogleMap;
+export default Homepage;
+
+const Layout = styled.div`
+  padding-top: 20em;
+  /* text-align: center;
+  display: flex;
+  flex-direction: column; */
+`
