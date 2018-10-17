d9dc27d9dd965b0d9cd49ca029ac269f3557e832
YAYAA <Apitchaya@Apitchaya.local>
2018-10-17 16:55:14 +0700

    first commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+
+class GoogleMap extends Component {
+  render() {
+    return (
+      <div>
+        Moovle!
+        <Button content='Click Here' />
+      </div>
+    );
+  }
+}
+
+export default GoogleMap;
