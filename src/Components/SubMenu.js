5368a53e61343d61bb74f8388598f13e5423b9e4
YAYAA <yayaa@Apitchaya.local>
2018-11-21 16:35:18 +0700

    change ui subMenu

--- src/Components/SubMenu.js
+++ src/Components/SubMenu.js
@@ -12,45 +12,85 @@ export default class SubMenu extends Component {
     const { activeItem } = this.state
 
     return (
-      <Menu fluid widths={1} text vertical>
-        <div class="ui internally celled grid">
-          <div class='row'>
-            <Menu.Item name='Movies' color='blue' active={activeItem === 'Movies'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
-              <TextFont>
-                Movies
-              </TextFont>
-            </Menu.Item>
-          </div>
-          <div class='row'>
-            <Menu.Item name='Genres' color='blue' active={activeItem === 'Genres'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
-              <TextFont>
-                Genres
-              </TextFont>
-            </Menu.Item>
-          </div>
-          <div class='row'>
-            <Menu.Item name='Director' color='blue' active={activeItem === 'Director'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
-              <TextFont>
-                Director
-              </TextFont>
-            </Menu.Item>
-          </div>
-          <div class='row'>
-            <Menu.Item name='Studio' color='blue' active={activeItem === 'Studio'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
-              <TextFont>
-                Studio
-              </TextFont>
-            </Menu.Item>
-          </div>
-          <div class='row'>
-            <Menu.Item name='Box Office' color='blue' active={activeItem === 'Box Office'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
-              <TextFont>
-                Box Office
-              </TextFont>
-            </Menu.Item>
-          </div>
-        </div>
-      </Menu>
+      // <Menu fluid widths={1} text vertical>
+      //   <div class="ui internally celled grid">
+      //     <div class='row'>
+      //       <Menu.Item name='Movies' color='blue' active={activeItem === 'Movies'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
+      //         <TextFont>
+      //           Movies
+      //         </TextFont>
+      //       </Menu.Item>
+      //     </div>
+      //     <div class='row'>
+      //       <Menu.Item name='Genres' color='blue' active={activeItem === 'Genres'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
+      //         <TextFont>
+      //           Genres
+      //         </TextFont>
+      //       </Menu.Item>
+      //     </div>
+      //     <div class='row'>
+      //       <Menu.Item name='Director' color='blue' active={activeItem === 'Director'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
+      //         <TextFont>
+      //           Director
+      //         </TextFont>
+      //       </Menu.Item>
+      //     </div>
+      //     <div class='row'>
+      //       <Menu.Item name='Studio' color='blue' active={activeItem === 'Studio'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
+      //         <TextFont>
+      //           Studio
+      //         </TextFont>
+      //       </Menu.Item>
+      //     </div>
+      //     <div class='row'>
+      //       <Menu.Item name='Box Office' color='blue' active={activeItem === 'Box Office'} onClick={this.handleItemClick} fitted='horizontally' as={Link} to={'/construction'}>
+      //         <TextFont>
+      //           Box Office
+      //         </TextFont>
+      //       </Menu.Item>
+      //     </div>
+      //   </div>
+      // </Menu>
+      <Menu secondary vertical>
+        <Menu.Item
+          name='Movies'
+          active={activeItem === 'Movies'}
+          onClick={this.handleItemClick}
+          // fitted='vertically' 
+          // position = 'center'
+          as={Link} 
+          to={'/construction'}
+        />
+        <Menu.Item
+          name='Genres'
+          active={activeItem === 'Genres'}
+          onClick={this.handleItemClick}
+          // fitted='horizontally' 
+          as={Link} 
+          to={'/construction'}
+        />
+        <Menu.Item
+          name='Director'
+          active={activeItem === 'Director'}
+          onClick={this.handleItemClick}
+          as={Link} 
+          to={'/construction'}
+        />
+        <Menu.Item
+          name='Studio'
+          active={activeItem === 'Studio'}
+          onClick={this.handleItemClick}
+          as={Link} 
+          to={'/construction'}
+        />
+        <Menu.Item
+          name='Box Office'
+          active={activeItem === 'Box Office'}
+          onClick={this.handleItemClick}
+          as={Link} 
+          to={'/construction'}
+        />
+    </Menu>
     )
   }
 }
